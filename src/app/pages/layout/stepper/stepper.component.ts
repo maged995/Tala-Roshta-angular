5360b501546d4f7d8f1fde09c2fcf3128587eb63
CoManDeR <comander@root.CoManDeR>
2022-05-05 08:45:50 +0200

    first Commit

---
+++ src/app/pages/layout/stepper/stepper.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'ngx-stepper',
+  templateUrl: 'stepper.component.html',
+  styleUrls: ['stepper.component.scss'],
+})
+export class StepperComponent implements OnInit {
+
+  firstForm: FormGroup;
+  secondForm: FormGroup;
+  thirdForm: FormGroup;
+
+  constructor() {
+  }
+
+  ngOnInit() {}
+
+}
